60166f6b1df4578e060cc51bfa08625a1cd869df
Creatlydev <creatlydev@gmail.com>
2024-08-09 09:51:19 -0500

    fix: disableCellEditing and wrapped in try cath

--- js/modules/cellActions.js
+++ js/modules/cellActions.js
@@ -20,7 +20,7 @@ export const isCellEditable = (cell) =>
   cell.getAttribute('contenteditable') === 'true'
 
 // Resalta la celda de entrada seleccionada y elimina la edición de otras celdas
-export const highlightInputCell = (cell, focusInIsCell= true) => {
+export const highlightInputCell = (cell, focusInIsCell = true) => {
   if (cell.getAttribute('role') === 'input') {
     const currentActiveCell = getCurrentActiveCell()
     if (currentActiveCell !== cell) {
@@ -30,9 +30,7 @@ export const highlightInputCell = (cell, focusInIsCell= true) => {
       disableCellEditing(cell)
 
       setCurrentActiveCell(cell)
-    }
-
-    else if (!focusInIsCell) {
+    } else if (!focusInIsCell) {
       disableCellEditing(cell, true)
     }
   }
@@ -45,11 +43,13 @@ export const setDomEditableCell = (cell) => {
 }
 
 // Elimina la edición de una celda y asegura que no haya otras celdas editables
-export const disableCellEditing = (cell, force= false) => {
+export const disableCellEditing = (cell, force = false) => {
   const currentEditableCell = getCurrentEditableCell()
 
-  if (currentEditableCell && currentEditableCell !== cell || force) {
-    currentEditableCell.removeAttribute('contenteditable')
+  if ((currentEditableCell && currentEditableCell !== cell) || force) {
+    try {
+      currentEditableCell.removeAttribute('contenteditable')
+    } catch (error) {}
   }
 }
 
@@ -59,15 +59,14 @@ export const moveFocusToNextCellOnEnter = (cell) => {
     cell.getAttribute('label-header-col') +
     (parseInt(cell.getAttribute('label-header-row')) + 1)
   let nextCell = $(`.cell[aria-label="${ariaLabelCurrentCellActive}"]`)
-  
+
   if (nextCell) {
     currentActiveCell.classList.remove('is-active')
     addClassCell(nextCell)
-    highlightHeaderCell({target: nextCell})
+    highlightHeaderCell({ target: nextCell })
     disableCellEditing(nextCell)
     setCurrentActiveCell(nextCell)
     nextCell.focus()
-    
   }
 }
 
