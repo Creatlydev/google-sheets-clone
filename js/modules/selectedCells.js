70e1674a8a25cc8b2c0a5ca5a64d5f3a439153b6
Creatlydev <creatlydev@gmail.com>
2024-08-11 20:04:32 -0500

    feat: working in selectingCells events

--- js/modules/selectedCells.js
+++ js/modules/selectedCells.js
@@ -1,47 +1,69 @@
-let isSelecting, startCell, currentEvent
-let startX, startY, finishX, finishY
+let isSelecting, startCell, finishCell, currentEvent
+let startX, startY, finishX, finishY, currentHoverCell
 let selectedCells = []
 let wrappedBox
 
-
 export const startSelectingCells = (event) => {
-    currentEvent = event
-    isSelecting = true
-    startCell = event.target
-    let boxCell = startCell.getBoundingClientRect()
-    startX = parseFloat(boxCell.left) + window.scrollX
-    startY = parseFloat(boxCell.top) + window.scrollY
-    finishX = event.clientX
-    finishY = event.clientY
-    
+  currentEvent = event
+  finishCell = startCell = event.target
+  let boxCell = startCell.getBoundingClientRect()
+  startX = parseFloat(boxCell.left) + window.scrollX
+  startY = parseFloat(boxCell.top) + window.scrollY
+
+  // Agregar celda inicial a la lista de seleccionadas
+  selectedCells.push(getRowAndColumn(startCell))
+
+  document.addEventListener('mousemove', selectingCells)
+  document.addEventListener('mouseup', finishSelectingCells)
+
+  document.addEventListener('mouseover', addCurrentHoverCell)
+}
+
+const addCurrentHoverCell = (event) => {
+  currentHoverCell = event.target
+  selectedCells.push(getRowAndColumn(currentHoverCell))
+}
+
+const selectingCells = (event) => {
+  document.body.style.userSelect = 'none'
+  if (!isSelecting && selectedCells.length <= 1) {
     wrappedBox = document.createElement('div')
     wrappedBox.classList.add('wrapped-cells')
-
-    document.addEventListener('mousemove', selectingCells)
-    document.addEventListener('mouseup', finishSelectingCells)
-
-}
-
-export const selectingCells = (event) => {
-    document.body.style.userSelect= 'none'
-
-    if (!isSelecting) return
     document.body.appendChild(wrappedBox)
+    console.log('ESTO SE EJECUTA SOLO LA PRIMERA VEZ')
+    return
+  }
 
-    finishX = event.pageX
-    finishY = event.pageY
-    let width = finishX - startX
-    let height = finishY - startY
-    
-    wrappedBox.style.top = `${startY}px`
-    wrappedBox.style.left = `${startX}px`
-    wrappedBox.style.width = `${width}px`
-    wrappedBox.style.height = `${height}px`
+  console.log('=======================================')
+
+  isSelecting = true
+
+  finishX = event.pageX
+  finishY = event.pageY
+  let width = finishX - startX
+  let height = finishY - startY
+
+  wrappedBox.style.top = `${startY}px`
+  wrappedBox.style.left = `${startX}px`
+  wrappedBox.style.width = `${width}px`
+  wrappedBox.style.height = `${height}px`
 }
 
-export const finishSelectingCells = () => {
-    document.body.style.userSelect= 'unset'
-    isSelecting = false
-    document.removeEventListener('mousemove', selectingCells)
-    document.removeEventListener('mouseup', finishSelectingCells)
+const finishSelectingCells = () => {
+  document.body.style.userSelect = 'unset'
+  isSelecting && document.body.removeChild(wrappedBox)
+  isSelecting = false
+  selectedCells = []
+  document.removeEventListener('mousemove', selectingCells)
+  document.removeEventListener('mouseup', finishSelectingCells)
+  document.removeEventListener('mouseover', addCurrentHoverCell)
+}
+
+// UTILITIES
+
+const getRowAndColumn = (cell) => {
+  return {
+    row: cell.getAttribute('label-header-row'),
+    column: cell.getAttribute('label-header-col'),
+  }
 }
