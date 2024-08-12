487c1c9a697b9bd1bbc2dbcae5960288ec95ddbf
Creatlydev <creatlydev@gmail.com>
2024-08-12 09:25:10 -0500

    refactor(cellSelectionBox): improve code structure, modularization, and add strategic comments

--- js/modules/cellSelectionBox.js
+++ js/modules/cellSelectionBox.js
@@ -1,74 +1,89 @@
 import { columnToNumber } from '../utils/columnToNumber.js'
 import { ROLE_INPUT } from '../utils/constants.js'
 
-let isSelecting, startCell, finishCell, currentEvent, currentHoverCell
+// Variables globales para el manejo de la selección de celdas
+let isSelecting, startCell, finishCell, currentHoverCell
 let selectedCells = []
-let wrappedBox, widthWrapped, heightWrapped
-let boxCell
+let wrappedBox, boxCell
 
+/**
+ * Inicia el proceso de selección de celdas
+ * @param {Event} event - Evento de clic en la celda inicial
+ */
 export const startSelectingCells = (event) => {
-  currentEvent = event
-  finishCell = startCell = event.target
+  startCell = finishCell = event.target
   boxCell = startCell.getBoundingClientRect()
 
-  // Agregar celda inicial a la lista de seleccionadas
+  // Agrega la celda inicial a la lista de celdas seleccionadas
   selectedCells.push(getRowAndColumn(startCell))
 
+  // Añade listeners para continuar la selección
   document.addEventListener('mousemove', selectingCells)
   document.addEventListener('mouseup', finishSelectingCells)
-
   document.addEventListener('mouseover', addCurrentHoverCell)
 }
 
+/**
+ * Agrega la celda actual al proceso de selección cuando se pasa sobre ella
+ * @param {Event} event - Evento mouseover
+ */
 const addCurrentHoverCell = (event) => {
-  console.log(event.target)
-  if (event.target.getAttribute('role') !== ROLE_INPUT) {
-    return
-  }
+  if (event.target.getAttribute('role') !== ROLE_INPUT) return
 
   currentHoverCell = event.target
   finishCell = event.target
   selectedCells.push(getRowAndColumn(currentHoverCell))
 
-  if (finishCell === startCell && isSelecting)
+  // Si la selección comenzó y se volvió a la celda inicial, se elimina el cuadro de selección
+  if (finishCell === startCell && isSelecting) {
     document.body.removeChild(wrappedBox)
-
-  if (finishCell !== startCell && !document.querySelector('.wrapped-cells')) {
-    wrappedBox = document.createElement('div')
-    wrappedBox.classList.add('wrapped-cells')
-    document.body.appendChild(wrappedBox)
   }
 
+  // Crea y agrega un cuadro de selección si aún no existe
+  if (finishCell !== startCell && !document.querySelector('.wrapped-cells')) {
+    wrappedBox = createWrappedBox()
+  }
+
+  // Actualiza el cuadro de selección con las nuevas dimensiones
   if (finishCell !== startCell) {
     setBoundingClientRect()
   }
 }
 
-const selectingCells = (event) => {
+/**
+ * Bloquea la selección de texto mientras se seleccionan celdas
+ * @param {Event} event - Evento mousemove
+ */
+const selectingCells = () => {
   document.body.style.userSelect = 'none'
   if (!isSelecting && selectedCells.length <= 1) return
   isSelecting = true
 }
 
+/**
+ * Finaliza el proceso de selección de celdas
+ */
 const finishSelectingCells = () => {
   document.body.style.userSelect = 'unset'
-  isSelecting && document.body.removeChild(wrappedBox)
-  isSelecting = false
-  selectedCells = []
-  document.removeEventListener('mousemove', selectingCells)
-  document.removeEventListener('mouseup', finishSelectingCells)
-  document.removeEventListener('mouseover', addCurrentHoverCell)
-}
-
-// UTILITIES
-
-const getRowAndColumn = (cell) => {
-  return {
-    row: parseInt(cell.getAttribute('label-header-row')),
-    column: cell.getAttribute('label-header-col'),
+  if (isSelecting) {
+    document.body.removeChild(wrappedBox)
   }
+  resetSelectionState()
 }
 
+/**
+ * Devuelve un objeto con la fila y la columna de una celda
+ * @param {Element} cell - Celda seleccionada
+ * @returns {Object} - Objeto con la fila y columna
+ */
+const getRowAndColumn = (cell) => ({
+  row: parseInt(cell.getAttribute('label-header-row')),
+  column: cell.getAttribute('label-header-col'),
+})
+
+/**
+ * Establece las dimensiones y posición del cuadro de selección en función de las celdas seleccionadas
+ */
 const setBoundingClientRect = () => {
   const { row: rowStart, column: columnStart } = getRowAndColumn(startCell)
   const { row: rowFinish, column: columnFinish } = getRowAndColumn(finishCell)
@@ -80,12 +95,12 @@ const setBoundingClientRect = () => {
     columnFinish
   )
 
-  const startYCell = calculateCellPosition(boxCell, positions.startPosY)
-  const startXCell = calculateCellPosition(boxCell, positions.startPosX)
+  const startYCell = boxCell[positions.startPosY] + window.scrollY
+  const startXCell = boxCell[positions.startPosX] + window.scrollX
 
   const boxFinishCell = finishCell.getBoundingClientRect()
-  const finishYCell = calculateCellPosition(boxFinishCell, positions.finishPosY)
-  const finishXCell = calculateCellPosition(boxFinishCell, positions.finishPosX)
+  const finishYCell = boxFinishCell[positions.finishPosY] + window.scrollY
+  const finishXCell = boxFinishCell[positions.finishPosX] + window.scrollX
 
   const height = Math.abs(finishYCell - startYCell)
   const width = Math.abs(finishXCell - startXCell)
@@ -93,6 +108,9 @@ const setBoundingClientRect = () => {
   applyStylesToWrappedBox(positions, startYCell, startXCell, height, width)
 }
 
+/**
+ * Determina las posiciones de inicio y fin del cuadro de selección
+ */
 const determineStartFinishPositions = (
   rowStart,
   columnStart,
@@ -129,13 +147,12 @@ const determineStartFinishPositions = (
   return { startPosY, startPosX, finishPosY, finishPosX }
 }
 
-const calculateCellPosition = (cellRect, position) => {
-  return parseFloat(cellRect[position]) + window.scrollY
-}
-
+/**
+ * Aplica los estilos calculados al cuadro de selección
+ */
 const applyStylesToWrappedBox = (positions, startY, startX, height, width) => {
-  let startPosY = positions.startPosY
-  let startPosX = positions.startPosX
+  let { startPosY, startPosX } = positions
+
   wrappedBox.style = ''
   wrappedBox.style[startPosY] =
     startPosY === 'bottom' ? `calc(100% - ${startY}px)` : `${startY}px`
@@ -143,4 +160,26 @@ const applyStylesToWrappedBox = (positions, startY, startX, height, width) => {
     startPosX === 'right' ? `calc(100% - ${startX}px)` : `${startX}px`
   wrappedBox.style.height = `${height}px`
   wrappedBox.style.width = `${width}px`
+}
+
+/**
+ * Crea y devuelve un nuevo elemento div para el cuadro de selección
+ * @returns {Element} - Elemento div creado
+ */
+const createWrappedBox = () => {
+  const box = document.createElement('div')
+  box.classList.add('wrapped-cells')
+  document.body.appendChild(box)
+  return box
+}
+
+/**
+ * Resetea el estado de la selección de celdas
+ */
+const resetSelectionState = () => {
+  isSelecting = false
+  selectedCells = []
+  document.removeEventListener('mousemove', selectingCells)
+  document.removeEventListener('mouseup', finishSelectingCells)
+  document.removeEventListener('mouseover', addCurrentHoverCell)
 }
